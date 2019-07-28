44c7747133089fa09c05c8b9fdd730311bf042b5
Narek Dallakyan <narek.dallakyan@binomial.xyz>
2019-07-28 20:42:37 +0400

    integrate app state listener logic.

--- app/src/main/java/com/sololearn/android/AppApplication.java
+++ app/src/main/java/com/sololearn/android/AppApplication.java
@@ -6,6 +6,7 @@ import com.sololearn.android.handler.appstate.AppLifeCycleListener;
 
 public class AppApplication extends Application implements AppLifeCycleListener {
     public static AppApplication appApplication;
+    public static boolean isBackground = false;
 
     @Override
     public void onCreate() {
@@ -15,11 +16,11 @@ public class AppApplication extends Application implements AppLifeCycleListener 
 
     @Override
     public void onBackground() {
-
+        isBackground = true;
     }
 
     @Override
     public void onForeground() {
-
+        isBackground = false;
     }
 }
